09666399ce79a369de803f5a15c86a7c21036ef5
JayMtz <81593531+JayMtz@users.noreply.github.com>
2022-05-04 14:32:03 -0500

    added tutorial provided function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+
+//tutorial provided function
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
 //this square function component is a controlled component by Board 
 function Square(props) {    
       return (
